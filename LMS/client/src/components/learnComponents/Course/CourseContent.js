d11d3ccf09ec4297a3986159eafe1b3147a82692
saravana <sravanafrancis16@gmail.com>
2024-02-15 23:14:09 +0530

    no change

--- LMS/client/src/components/learnComponents/Course/CourseContent.js
+++ LMS/client/src/components/learnComponents/Course/CourseContent.js
@@ -1,9 +1,30 @@
-
+import { Box, Card, CardActionArea, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import video from "./Video/jsc.mp4"
 
 
 const CourseContent = () =>{
     return(
-        <h1>Actual course page</h1>
+        <Box>
+        <Card >
+        <CardHeader
+            title={"First Video"}
+        />
+
+        <CardActionArea>
+            <CardMedia
+                component='video'
+                image={video}
+                autoplay
+            />
+            <CardContent>
+
+                <Typography variant="body2" color="textSecondary" component="p">
+                    Some Text
+                </Typography>
+            </CardContent>
+        </CardActionArea>
+    </Card>
+        </Box>
     )
 }
 export default CourseContent;
