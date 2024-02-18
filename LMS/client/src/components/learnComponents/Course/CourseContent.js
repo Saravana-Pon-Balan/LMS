a281738801ee12b664a24d92e9dd97adfae9a53e
saravana <sravanafrancis16@gmail.com>
2024-02-18 21:55:49 +0530

    docs added

--- LMS/client/src/components/learnComponents/Course/CourseContent.js
+++ LMS/client/src/components/learnComponents/Course/CourseContent.js
@@ -1,30 +1,29 @@
-import { Box, Card, CardActionArea, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
-import video from "./Video/jsc.mp4"
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import video from './Video/jsc.mp4';
+export default function MediaControlCard() {
 
-
-const CourseContent = () =>{
-    return(
-        <Box>
-        <Card >
-        <CardHeader
-            title={"First Video"}
-        />
-
-        <CardActionArea>
-            <CardMedia
-                component='video'
-                image={video}
-                autoplay
-            />
-            <CardContent>
-
-                <Typography variant="body2" color="textSecondary" component="p">
-                    Some Text
-                </Typography>
-            </CardContent>
-        </CardActionArea>
-    </Card>
-        </Box>
-    )
+  return (
+      
+      <CardMedia
+        component="video"
+        sx={{ width: "100%",height:"100%" }}
+        image={video}
+        controls
+        alt="Live from space album cover"
+      />
+  );
 }
-export default CourseContent;
+user management 
+course management
+compiler
+recommendation System
